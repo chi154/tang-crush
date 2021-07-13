db6603915429697d5a6ca2f974f94d7f84a8546a
chi154 <87368568+chi154@users.noreply.github.com>
2021-07-13 18:13:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle:cháu à!',
+    introTitle:'cháu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao a cứ đi nhầm đường
     Lạc vào tim e lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'cc ấn vào đây nè
-    title: 'Phải chăng anh đã y ngay từ cái nhìn đầu tiên
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy
-    btnYes: 'yêu emmmmmm
-    btnNo: 'Không, em trai à :))
-    question:'Trên thế giới hơn 7 tỉ người mà sao a lại yêu em,
+    btnIntro: 'cc ấn vào đây nè',
+    title: 'Phải chăng anh đã y ngay từ cái nhìn đầu tiên',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy',
+    btnYes: 'yêu emmmmmm',
+    btnNo: 'Không, em trai à :))',
+    question:'Trên thế giới hơn 7 tỉ người mà sao a lại yêu em <3',
     btnReply: 'Gửi cho em <3;
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Chiiiinhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Yêu Chiiii nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h làm chi',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/hlcchii
+    messLink: 'https://www.facebook.com/hlcchii'
 }
